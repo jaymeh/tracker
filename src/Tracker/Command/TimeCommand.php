bd1efda8e456a846104da3d45aaaeb348817d0ea
Jamie Sykes <jamie.sykes@creode.co.uk>
2018-07-10 10:46:23 +0100

    Fixes bug with date type if one is not selected or allowed.

--- src/Tracker/Command/TimeCommand.php
+++ src/Tracker/Command/TimeCommand.php
@@ -50,7 +50,7 @@ class TimeCommand extends Command
             return $output->writeln('<error>Couldn\'t determine workspace id</error>');
         }
 
-        $inputted_dates = $this->checkDateType($input, $date_type);
+        $inputted_dates = $this->checkDateType($input, $date_type, $output);
 
         // If we can't get any data from the type we have then throw an error.
         if(!$inputted_dates) {
@@ -363,7 +363,7 @@ class TimeCommand extends Command
         return $workspace_id;
     }
 
-    private function checkDateType($input, $date_type) {
+    private function checkDateType($input, $date_type, $output) {
         // Check the day
         switch($date_type) {
             case 'today':
